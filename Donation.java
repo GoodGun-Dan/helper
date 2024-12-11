17c55c8fa64d079ca21dadff4bd5343442a04905
Ahsan <ahsan.rusdin@student.president.ac.id>
2024-12-11 12:10:38 +0800

    Donation.java

--- Donation.java
+++ Donation.java
@@ -1,1 +1,41 @@
+package com.example.helper;
 
+public class Donation {
+    private String recipientUid;
+    private String recipientName;
+    private String donationDate;
+
+    public Donation() {
+        // Default constructor required for Firebase
+    }
+
+    public Donation(String recipientUid, String recipientName, String donationDate) {
+        this.recipientUid = recipientUid;
+        this.recipientName = recipientName;
+        this.donationDate = donationDate;
+    }
+
+    public String getRecipientUid() {
+        return recipientUid;
+    }
+
+    public void setRecipientUid(String recipientUid) {
+        this.recipientUid = recipientUid;
+    }
+
+    public String getRecipientName() {
+        return recipientName;
+    }
+
+    public void setRecipientName(String recipientName) {
+        this.recipientName = recipientName;
+    }
+
+    public String getDonationDate() {
+        return donationDate;
+    }
+
+    public void setDonationDate(String donationDate) {
+        this.donationDate = donationDate;
+    }
+}
